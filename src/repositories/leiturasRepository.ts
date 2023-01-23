7f453fc00bf3439719e95e4893cd7ee7398f8565
Analice <anadias.alice@gmail.com>
2023-01-23 14:59:23 -0300

    feat: deletarLeitura repository

--- src/repositories/leiturasRepository.ts
+++ src/repositories/leiturasRepository.ts
@@ -4,3 +4,11 @@ import { criarLeituraType } from "../Interfaces/leiturasInterfaces";
 export async function inserirLeitura ( data: criarLeituraType ) {
     return await db.minhasLeituras.create({data});
 }
+
+export async function deletarLeitura (leituraId: number) {
+    return await db.minhasLeituras.delete({
+        where: {
+            id: leituraId
+        }
+    })
+}
