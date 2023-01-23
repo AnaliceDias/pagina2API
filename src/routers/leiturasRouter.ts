0c00b453775f64a9920c97e294e7bf3ce35f5836
Analice <anadias.alice@gmail.com>
2023-01-23 16:10:23 -0300

    feat: +put /minhas-leituras/:leituraId/onde-parei

--- src/routers/leiturasRouter.ts
+++ src/routers/leiturasRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { apagarLeitura, criarLeitura, listarLeituras } from "../controllers/leiturasControllers";
+import { apagarLeitura, atualizarLeitura, criarLeitura, listarLeituras } from "../controllers/leiturasControllers";
 import validarSchema from "../middlewares/validarSchema";
-import { criarLeituraSchema } from "../schemas/leiturasSchemas";
+import { atualizarPagSchema, criarLeituraSchema } from "../schemas/leiturasSchemas";
 
 const leiturasRouter = Router();
 
@@ -9,5 +9,6 @@ leiturasRouter
     .post("/minhas-leituras", validarSchema(criarLeituraSchema), criarLeitura)
     .delete("/minhas-leituras/:leituraId", apagarLeitura)
     .get("/minhas-leituras", listarLeituras)
+    .put("/minhas-leituras/:leituraId/onde-parei", validarSchema(atualizarPagSchema), atualizarLeitura)
 
 export default leiturasRouter;
