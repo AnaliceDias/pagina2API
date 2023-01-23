e2f8718db7ba9702afe0b568da674494096d6985
Analice <anadias.alice@gmail.com>
2023-01-23 15:06:10 -0300

    feat: +statusType +formatoType +criarLeituraType

---
+++ src/Interfaces/leiturasInterfaces.ts
@@ -0,0 +1,12 @@
+import { Status , Formatos } from "@prisma/client";
+
+export type statusType = Omit< Status, "id" >
+export type formatoType = Omit< Formatos, "id" >
+export type criarLeituraType = {
+    titulo: string,
+    autor: string,
+    edicao: string,
+    numeroDePag: string,
+    formatoId: number,
+    statusId: number
+}
