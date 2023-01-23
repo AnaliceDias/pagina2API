5a042879a14d18a28947f900735c3549fd87705f
Analice <anadias.alice@gmail.com>
2023-01-23 17:54:04 -0300

    feat: +get /status +get /formatos

--- src/routers/leiturasRouter.ts
+++ src/routers/leiturasRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { apagarLeitura, atualizarLeitura, criarLeitura, listarLeituraPeloId, listarLeituras } from "../controllers/leiturasControllers";
+import { apagarLeitura, atualizarLeitura, criarLeitura, listarFormatos, listarLeituraPeloId, listarLeituras, listarStatus } from "../controllers/leiturasControllers";
 import validarSchema from "../middlewares/validarSchema";
 import { atualizarPagSchema, criarLeituraSchema } from "../schemas/leiturasSchemas";
 
@@ -11,5 +11,7 @@ leiturasRouter
     .get("/minhas-leituras", listarLeituras)
     .get("/minhas-leituras/:id", listarLeituraPeloId)
     .put("/minhas-leituras/:leituraId/pag-atual", validarSchema(atualizarPagSchema), atualizarLeitura)
+    .get("/status", listarStatus)
+    .get("/formatos", listarFormatos)
 
 export default leiturasRouter;
