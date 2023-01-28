889dbb732b2cdbdcdd6dbc66262e557f87605777
Analice <anadias.alice@gmail.com>
2023-01-28 00:52:16 -0300

    feat: listarObras service

--- src/services/acervoServices.ts
+++ src/services/acervoServices.ts
@@ -5,6 +5,26 @@ async function listarAutores(){
     return result;
 }
 
+async function listarObras() {
+    const result = await acervoRepository.buscarObras();
+    let leituras = [];
+
+    result.map((leitura) => {
+        let listaAutores = [];
+        leitura.obrasAutores.map((autores) => {
+            listaAutores.push(autores.autor.autor)
+        })
+        leituras.push({
+            id: leitura.id,
+            titulo: leitura.titulo,
+            autores: [...listaAutores],
+        })
+    })
+
+    return leituras;
+}
+
 export const acervoService = {
     listarAutores,
+    listarObras,
 }
