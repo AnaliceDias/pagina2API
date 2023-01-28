8ab744a62996974de12a09779a39ae392add1a94
Analice <anadias.alice@gmail.com>
2023-01-28 00:13:04 -0300

    feat: +use acervoRouter

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import acervoRouter from "./acervoRouter";
 import leiturasRouter from "./leiturasRouter";
 
 const router = Router();
 
-router.use(leiturasRouter)
+router.use(leiturasRouter);
+router.use(acervoRouter);
 
 export default router;
