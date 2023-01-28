88a749a576b5e6ea386fd10ee78a94d47c606b2c
Analice <anadias.alice@gmail.com>
2023-01-27 22:19:58 -0300

    feat: +seed das tabelas obras, autores e obrasAutores

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,23 +2,81 @@ import db from "../src/config/database";
 import { formatoType, statusType } from "../src/Interfaces/leiturasInterfaces";
 
 async function main() {
-    const status: statusType[] = [
+    const status = [
         {status: "Quero Ler"},
         {status: "Lendo"},
         {status: "Abandonei"},
         {status: "Terminei"},
     ]
 
-    const formatos: formatoType[] = [
+    const formatos = [
         {formato: "Digital"},
         {formato: "Físico"},
     ]
 
+    const autores = [
+        {autor: "C.S. Lewis"},
+        {autor: "John Green"},
+        {autor: "David Levithan"},
+        {autor: "Jhon Green"},
+        {autor: "Ilana Casoy"},
+        {autor: "Raphael Montes"},
+        {autor: "Mary Shelley"},
+    ]
+
+    const obras = [
+        {titulo: "O sobrinho do mago"},
+        {titulo: "Will & Will"},
+        {titulo: "A culpa é das estrelas"},
+        {titulo: "Bom dia, Verônica"},
+        {titulo: "Frankenstein"},
+    ]
+
+    const obrasAutores = [
+        {
+            obraId: 1,
+            autorId: 1,
+        },
+        {
+            obraId: 2,
+            autorId: 2,
+        },
+        {
+            obraId: 2,
+            autorId: 3,
+        },
+        {
+            obraId: 3,
+            autorId: 2,
+        },
+        {
+            obraId: 4,
+            autorId: 5,
+        },
+        {
+            obraId: 4,
+            autorId: 6,
+        },
+        {
+            obraId: 5,
+            autorId: 7,
+        },
+    ]
+
     await db.status.createMany({ data: status });
     await db.formatos.createMany({ data: formatos });
-
-    await db.$disconnect();
+    await db.autores.createMany({data: autores});
+    await db.obras.createMany({data: obras});
+    await db.obrasAutores.createMany({data:obrasAutores});
 
 }
 
-main();
+main().then(() => {
+console.log("DB populado com sucesso");
+})
+.catch((e) => {
+    console.log(e);
+})
+.finally(() => {
+    db.$disconnect();
+});
